297af1f16439ff95c6990813f287fea98ba777b0
Jiayu0330 <47224691+Jiayu0330@users.noreply.github.com>
2019-02-21 19:57:58 -0500

    Create webscraper4.js

---
+++ webscraper4.js
@@ -0,0 +1,7 @@
+var script = document.createElement("script");
+script.src= "https://d3js.org/d3.v5.js";
+var body = document.getElementsByTagName('body')[0];
+body.after(script);
+d3.selectAll("a").nodes().forEach(function(e){
+  console.log(e.innerText);
+});
